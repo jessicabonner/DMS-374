c2dfb44c1e58f86e90ecfdf1aa80c1f1a677bdcd
JESSICA\Jessica <jessicabonner@utexas.edu>
2017-11-14 09:27:36 -0600

    Changed to fit coding standards
    Changed spacing and indentation so now it first our coding and io standards
    
    ***ALSO had to change variable names to fit standards which means you'll have to change the names in your database table for the application to work

--- DMS_Connect2.php
+++ DMS_Connect2.php
@@ -1,60 +1,66 @@
 <?php
 
+//link to file containing database connection string
 require 'DMS_db.php';
 
 
-
-$FName=$_POST['FName'];
-$MName=$_POST['MName'];
-$LName=$_POST['LName'];
+//create variables from those submitted through application form (DMS_Apply.html)
+$first_name=$_POST['first_name'];
+$middle_name=$_POST['middle_name'];
+$last_name=$_POST['last_name'];
 $EID=$_POST['EID'];
-$Address=$_POST['address'];
-$City=$_POST['city'];
-$State=$_POST['state'];
-$Zip=$_POST['zip'];
-$Phone=$_POST['phone'];
-$Email=$_POST['email'];
+$address=$_POST['address'];
+$city=$_POST['city'];
+$state=$_POST['state'];
+$zip_code=$_POST['zip_code'];
+$phone=$_POST['phone'];
+$email=$_POST['email'];
 $employment=$_POST['employment'];
 $classification=$_POST['classification'];
-$Degree_Type=$_POST['Degree_Type'];
-$Major=$_POST['Major'];
-$Major_2=$_POST['Major_2'];
-$Worked_at_dms=$_POST['Worked_at_dms'];
-$Volunteered_at_seton=$_POST['Volunteered_at_seton'];
-$Car=$_POST['Car'];
-$Semester_commitment=$_POST['Semester_commitment'];
-$EssayQ1=$_POST['EssayQ1'];
-$EssayQ2=$_POST['EssayQ2'];
-$EssayQ3=$_POST['EssayQ3'];
-$EssayQ4=$_POST['EssayQ4'];
-$EssayQ5=$_POST['EssayQ5'];
+$degree_type=$_POST['degree_type'];
+$major=$_POST['major'];
+$major_2=$_POST['major_2'];
+$worked_at_dms=$_POST['worked_at_dms'];
+$volunteered_at_seton=$_POST['volunteered_at_seton'];
+$car=$_POST['car'];
+$semester_commitment=$_POST['semester_commitment'];
+$essay_q1=$_POST['essay_q1'];
+$essay_q2=$_POST['essay_q2'];
+$essay_q3=$_POST['essay_q3'];
+$essay_q4=$_POST['essay_q4'];
+$essay_q5=$_POST['essay_q5'];
 
-$Availability= implode(',', $_POST['Availability_list']);
+//turn availability_list into a string 
+$availability= implode(',', $_POST['availability_list']);
 
+
+
+//create sql code to insert record into application table
+$sql = "INSERT INTO application (first_name, middle_name, last_name, EID, address, city, state, zip_code, phone, email, employment,
+classification, degree_type, major, major_2, worked_at_dms, volunteered_at_seton, car, semester_commitment, essay_q1,
+essay_q2, essay_q3, essay_q4, essay_q5, availability) VALUES ('$first_name', '$middle_name', '$last_name', '$EID', '$address', '$city',
+'$state', '$zip_code', '$phone', '$email', '$employment', '$classification', '$degree_type', '$major', '$major_2', '$worked_at_dms',
+'$volunteered_at_seton', '$car', '$semester_commitment', '$essay_q1', '$essay_q2', '$essay_q3', '$essay_q4', '$essay_q5', '$availability')";
+
+//commit sql code
+$result= mysqli_query($dbc, $sql) or die(mysqli_error($dbc));
 
 
 /*
-$sql = "INSERT INTO application (FName, MName, LName, EID, Address, City, State, Zip, Phone, Email, employment,
-Classification, Degree_Type, Major, Major_2, Worked_at_dms, Volunteered_at_seton, Car, Semester_commitment, EssayQ1,
-EssayQ2, EssayQ3, EssayQ4, EssayQ5, Availability) VALUES ('$FName', '$MName', '$LName', '$EID', '$Address', '$City',
-'$State', '$Zip', '$Phone', '$Email', '$employment', '$classification', '$Degree_Type', '$Major', '$Major_2', '$Worked_at_dms',
-'$Volunteered_at_seton', '$Car', '$Semester_commitment', '$EssayQ1', '$EssayQ2', '$EssayQ3', '$EssayQ4', '$EssayQ5', '$Availability')";
+if ($sql=$mysqli -> prepare("INSERT INTO application (first_name, middle_name, last_name, EID, address, city, state, zip_code, phone, email, employment,
+classification, degree_type, major, major_2, worked_at_dms, volunteered_at_seton, car, semester_commitment, essay_q1,
+essay_q2, essay_q3, essay_q4, essay_q5, availability) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")){
 
-$result= mysqli_query($dbc, $sql) or die(mysqli_error($dbc));
-*/
-
-if ($sql=$mysqli -> prepare("INSERT INTO application (FName, MName, LName, EID, Address, City, State, Zip, Phone, Email, employment,
-Classification, Degree_Type, Major, Major_2, Worked_at_dms, Volunteered_at_seton, Car, Semester_commitment, EssayQ1,
-EssayQ2, EssayQ3, EssayQ4, EssayQ5, Availability) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")){
-
-	$sql->bind_param("sssssssssssssssssssssssss",$FName, $MName, $LName, $EID, $Address, $City,
-$State, $Zip, $Phone, $Email, $employment, $classification, $Degree_Type, $Major, $Major_2, $Worked_at_dms,
-$Volunteered_at_seton, $Car, $Semester_commitment, $EssayQ1, $EssayQ2, $EssayQ3, $EssayQ4, $EssayQ5, $Availability);
+	$sql->bind_param("sssssssssssssssssssssssss",$first_name, $middle_name, $last_name, $EID, $address, $city,
+	$state, $zip_code, $phone, $email, $employment, $classification, $degree_type, $major, $major_2, $worked_at_dms,
+	$volunteered_at_seton, $car, $semester_commitment, $essay_q1, $essay_q2, $essay_q3, $essay_q4, $essay_q5, $availability);
 
 	$sql-> execute();
 
 	$sql->close();
 }
+*/
+
 
 echo '<h1 style= "text-align:center;color:#BF5700">Your application has been received! Thanks for applying!</h1>';
 
