b2f1579b0bf378c491695f5a6106cd2651d0f442
Tannia Rodriguez <tanniarodriguez@github.com>
2017-11-19 17:39:48 -0600

    Removed Essays
    Removed essay questions from student account form

--- DMS_Connect2.php
+++ DMS_Connect2.php
@@ -24,31 +24,29 @@ $worked_at_dms=$_POST['worked_at_dms'];
 $volunteered_at_seton=$_POST['volunteered_at_seton'];
 $car=$_POST['car'];
 $semester_commitment=$_POST['semester_commitment'];
-$essay_q1=$_POST['essay_q1'];
-$essay_q2=$_POST['essay_q2'];
-$essay_q3=$_POST['essay_q3'];
-$essay_q4=$_POST['essay_q4'];
-$essay_q5=$_POST['essay_q5'];
+//$essay_q1=$_POST['essay_q1'];
+//$essay_q2=$_POST['essay_q2'];
+//$essay_q3=$_POST['essay_q3'];
+//$essay_q4=$_POST['essay_q4'];
+//$essay_q5=$_POST['essay_q5'];
 
 //turn availability_list into a string 
 $availability= implode(',', $_POST['availability_list']);
 
+$user_id = 1;
 
 
 //prepare SQL statement to prevent SQL injection
-$stmt = $dbc-> prepare('INSERT INTO application (first_name, middle_name, last_name, EID, address, city, state, zip_code, phone, email, employment,
-classification, degree_type, major, major_2, worked_at_dms, volunteered_at_seton, car, semester_commitment, essay_q1,
-essay_q2, essay_q3, essay_q4, essay_q5, availability) VALUES (:first_name, :middle_name, :last_name, :EID, :address, :city, :state, :zip_code, :phone, 
-:email, :employment, :classification, :degree_type, :major, :major_2, :worked_at_dms, :volunteered_at_seton, :car, :semester_commitment, :essay_q1,
-:essay_q2, :essay_q3, :essay_q4, :essay_q5, :availability)');
+$stmt = $dbc-> prepare('INSERT INTO student_info (user_id, first_name, middle_name, last_name, EID, address, city, state, zip_code, phone, email, employment,
+classification, degree_type, major, major_2, worked_at_dms, volunteered_at_seton, car, semester_commitment, availability) VALUES (:user_id, :first_name, :middle_name, :last_name, :EID, :address, :city, :state, :zip_code, :phone, 
+:email, :employment, :classification, :degree_type, :major, :major_2, :worked_at_dms, :volunteered_at_seton, :car, :semester_commitment, :availability)');
 
 
 //bind variables to prepared statement and execute
-$stmt->execute(array('first_name' => $first_name, 'middle_name' => $middle_name, 'last_name' => $last_name,'EID' => $EID,'address' => $address,'city' => $city,
+$stmt->execute(array('user_id' => $user_id, 'first_name' => $first_name, 'middle_name' => $middle_name, 'last_name' => $last_name,'EID' => $EID,'address' => $address,'city' => $city,
 'state' => $state,'zip_code' => $zip_code,'phone' => $phone,'email' => $email,'employment' => $employment,'classification' => $classification,
 'degree_type' => $degree_type,'major' => $major,'major_2' => $major_2,'worked_at_dms' => $worked_at_dms,'volunteered_at_seton' => $volunteered_at_seton,
-'car'=>$car,'semester_commitment' => $semester_commitment,'essay_q1' => $essay_q1,'essay_q2' => $essay_q2,'essay_q3' => $essay_q3,'essay_q4' => $essay_q4,
-'essay_q5' => $essay_q5,'availability' => $availability ));
+'car'=>$car,'semester_commitment' => $semester_commitment,'availability' => $availability ));
 
 	
 
