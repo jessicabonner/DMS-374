2ceada481ac7b35c87c37e32c246acda3ed5737c
MiguelMartinez <miguelmartinez@utexas.edu>
2017-11-20 02:11:48 -0600

    I don't know why I can't get it to go more to the right

--- DMS_CreateApplication.php
+++ DMS_CreateApplication.php
@@ -1,11 +1,130 @@
-<!DOCTYPE html>
-<html>
-
+<!doctype html>
+<html lang="en" dir="ltr">
 <head>
-	<link href='application.css' type='text/css' rel='stylesheet'>
+	<link href='./application.css' type='text/css' rel='stylesheet'>
 	<script src="https://code.jquery.com/jquery-3.1.1.js"></script>
 </head>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="apple-touch-icon" sizes="180x180" href="/sites/all/themes/phase2_theme1/img/favicon/apple-touch-icon-180x180.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="/sites/all/themes/phase2_theme1/img/favicon/apple-touch-icon-152x152.png" />
+<link rel="apple-touch-icon" sizes="144x144" href="/sites/all/themes/phase2_theme1/img/favicon/apple-touch-icon-144x144.png" />
+<link rel="icon" href="/sites/all/themes/phase2_theme1/img/favicon/favicon.ico" />
+<!--[if IE]><link rel="shortcut icon" href="/sites/all/themes/phase2_theme1/img/favicon/favicon.ico" />
+<![endif]--><meta name="apple-mobile-web-app-title" content="UT Austin" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+<meta name="msapplication-TileImage" content="/sites/all/themes/phase2_theme1/img/favicon/apple-touch-icon-144x144.png" />
+<meta name="msapplication-TileColor" content="#bf5700" />
+<link rel="apple-touch-icon" sizes="120x120" href="/sites/all/themes/phase2_theme1/img/favicon/apple-touch-icon-120x120.png" />
+<link rel="apple-touch-icon" sizes="76x76" href="/sites/all/themes/phase2_theme1/img/favicon/apple-touch-icon-76x76.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="/sites/all/themes/phase2_theme1/img/favicon/apple-touch-icon-114x114.png" />
+<link rel="apple-touch-icon" sizes="57x57" href="/sites/all/themes/phase2_theme1/img/favicon/apple-touch-icon-57x57.png" />
+<link rel="apple-touch-icon" href="/sites/all/themes/phase2_theme1/img/favicon/apple-touch-icon.png" />
+<link rel="apple-touch-icon" sizes="60x60" href="/sites/all/themes/phase2_theme1/img/favicon/apple-touch-icon-60x60.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="/sites/all/themes/phase2_theme1/img/favicon/apple-touch-icon-72x72.png" />
+<meta name="description" content="Dell Medical School at The University of Texas at Austin is the first med school in decades to be built from the ground up at a Tier 1 research university." />
+<meta name="robots" content="follow, index" />
+<meta name="generator" content="Drupal 7 (http://drupal.org)" />
+<link rel="canonical" href="https://dellmed.utexas.edu" />
+<link rel="shortlink" href="https://dellmed.utexas.edu" />
+  <title>Dell Medical School | The University of Texas at Austin</title>
+  <link type="text/css" rel="stylesheet" href="https://dellmed.utexas.edu/sites/default/files/css/css_xE-rWrJf-fncB6ztZfd2huxqgxu4WO-qwma6Xer30m4.css" media="all" />
+<link type="text/css" rel="stylesheet" href="https://dellmed.utexas.edu/sites/default/files/css/css_tKJ8QKUw8OLBfSpVi3r2kqhI0EM9KvnZzuv9rNVL1dE.css" media="all" />
+<link type="text/css" rel="stylesheet" href="https://dellmed.utexas.edu/sites/default/files/css/css_ObkY4Fv7biAuohhzB1p-hgy32GQxKG4rzg9E0b42Xo0.css" media="all" />
+<link type="text/css" rel="stylesheet" href="https://dellmed.utexas.edu/sites/default/files/css/css_YytGlvj-rOSj7aCuw23k0KHgv0uW_7b2NUNxl_vdSsM.css" media="all" />
+<style type="text/css" media="all">
+/*--><![CDATA[/*><!--*/
+#main-nav li a{font-family:open_sans;}
+.UT-page{margin-top:50px;}
 
+/*]]>*/
+</style>
+<link type="text/css" rel="stylesheet" href="https://dellmed.utexas.edu/sites/default/files/css/css_a-iX8Z0TGtqOTsvj7qkSGIxcKy1DQVow38xs9TgeR0g.css" media="all" />
+  <script type="text/javascript" src="https://dellmed.utexas.edu/sites/default/files/js/js_nGsGFAVr6D4cI4gpxlZHFJ7PJaRNEW3-0MdbO3ITML0.js"></script>
+</head>
+
+<body class="html front not-logged-in no-sidebars page-node"  >
+  <div id="skip-link">
+    <a href="#ut-page-content" class="element-invisible element-focusable">Skip to main content</a>
+  </div>
+      <header class="UT-header theme1">
+
+    <div class="container container-logo-p2">
+      <div class="row">
+        <div class="column small-12">
+           <div class="p2-logo">
+             <a href="/" class="main-logo"><img src ="Dell_Medical_School_logo.png" alt="Home" /></a>
+          </div>
+        </div>
+      </div>
+      <a href="#" class="UT-nav-hamburger icon-menu" id="menu-icon"><span class="hiddenText">Menu</span></a>
+    </div> <!-- container-logo -->
+
+    <div class="nav-overlay" id="nav-overlay"></div>
+    <div class="nav-wrapper" role="navigation">
+
+      <div class="container container-topnav">
+        <div class="row">
+          <div class="column small-12">
+            <div class="topnav">
+              <div class="hide-for-large-up">
+                <ul class="topnav-constituents" role="menu"><li class="nav-item" role="menuitem"><a href="/maps" id="cta-button-style1" class="nav-link">Maps</a></li>
+<li class="nav-item" role="menuitem"><a href="/philanthropy" id="cta-button-style2" class="nav-link">Give</a></li>
+<li class="nav-item" role="menuitem"><a href="/events" class="nav-link">Events</a></li>
+<li class="nav-item" role="menuitem"><a href="/in-the-news" class="nav-link">News</a></li>
+</ul>              </div>
+              <div class="parent-banner-links">
+                <h2 class="UT-secondary-logo">
+                  <a href="http://www.utexas.edu" class="logo-link"><img src="Texas_logo.png" alt="UTexas Home" /><br></a>
+                </h2>
+                              </div>
+
+              <div class="hide-for-large-up">
+                <div class="parent-links" id="parents">
+                  <a href="http://www.utexas.edu">The University of Texas at Austin</a>
+                                  </div>
+
+                                  <a href="/" class="current-directory" id="show-parents"><span class="name">Dell Medical School</span><span class="toggle"></span></a>
+                              </div>
+                      <br>
+                    <br>
+                  <br>
+            </div>
+          </div>
+        </div>
+      </div> <!-- container-topnav -->
+
+      <div class="container container-nav container-nav-phase2">
+        <div class="row">
+          <div class="column small-12">
+            <nav>
+              <ul class="nav" id="main-nav" role="menu">
+            <li class="nav-item" role="menuitem">
+        <a href="#" class="nav-link has-child nolink">Home</a>                  <div class="sub-nav-wrapper">
+          </div>
+              </li>
+                <li class="nav-item" role="menuitem">
+        <a href="#" class="nav-link has-child nolink">New Program</a>                  <div class="sub-nav-wrapper">
+          </div>
+              </li>
+                <li class="nav-item" role="menuitem">
+        <a href="#" class="nav-link has-child nolink">New Application</a>                  <div class="sub-nav-wrapper">
+          </div>
+              </li>
+                <li class="nav-item" role="menuitem">
+            </div>
+          </div>
+              </li>
+                  </ul>
+            </nav>
+          </div>
+        </div>
+      </div> <!-- container-nav-phase2 -->
+
+    </div> <!-- nav-wrapper -->
+
+		<br>
+		<br>
 
 <body>
 	<!--this form will post to DMS_connect in order to submit data to DB-->
@@ -17,18 +136,18 @@
 			<?php
 				//link to file containing database connection string
 				require 'DMS_db.php';
-				
+
 				$sql="SELECT program_id, name_of_program FROM programs";
 				$stmt=$dbc->prepare($sql);
 				$stmt->execute();
 				$programs= $stmt->fetchAll();
 			?>
-			
+
 			<tr>
-			
+
 				<td>Please select which program this application is for</td>
 			</tr>
-			
+
 			<tr>
 				<td>
 					<select name="program_id" required>
@@ -38,18 +157,18 @@
 					</select>
 				</td>
 			</tr>
-			
+
 			<!--break-->
 			<tr>
 				<td><br></td>
 			</tr>
-			
+
 			<tr>
-			
+
 				<td>Please enter the year and semester that this application is for</td>
 			</tr>
-			
-			
+
+
 			<tr>
 				<td>
 					<select name="term" required>
@@ -61,43 +180,43 @@
 					</select>
 				</td>
 			</tr>
-			
+
 			<tr>
-				
+
 				<td>
 					<input type="text" placeholder="ex: 2017" name="year" maxlength="4" pattern="^[0-9]*$" required/>
 				</td>
 			</tr>
-			
+
 			<!--break-->
 			<tr>
 				<td><br></td>
 			</tr>
-			
+
 			<!--text box for number of programs to create-->
 			<tr>
 				<td>Please enter the number of custom questions that you wish to create</td>
 			</tr>
 			<tr>
 				<td><input type="text" name="number_unique_questions" id="number_unique_questions" size="20" maxlength="2" pattern="^[0-9]+$" required/></td>
-				
+
 			</tr>
 
 
 		</table>
-		
-		
+
+
 		<!--break-->
 		<tr>
 			<td><br></td>
-		</tr> 
-		
+		</tr>
+
 		<table id="unique_questions_table">
-			
-		
+
+
 		</table>
-		
-		
+
+
 		<!--break-->
 		<tr>
 			<td><br></td>
@@ -106,33 +225,32 @@
 
 		<!--submit button. Will post info.-->
 		<td colspan="1" style="text-align: center; float: center;"><input type="submit" value="Submit" name="submit"/> </td>
-		
-		
-		
-	
-	
-	
+
+
+
+
+
+
 	<script>
-	
+
 		$("#number_unique_questions").keyup(function(){
 			//$("p").hide();
-			
-			
+
+
 			var question_number = $("#number_unique_questions").val();
-			
+
 			$("#unique_questions_table").empty();
 			$("#unique_questions_table").prepend("Enter the unique questions below");
 			while (question_number>0){
-				
+
 				var text = $("<tr></tr>").html("<input type='text' name='list_unique_questions[]' id='unique_question' required/>");
 				$("#unique_questions_table").append(text);
 				question_number-=1;
 			}
-			
+
 		});
-	
+
 	</script>
-	
+
 	</body>
 </html>
-
