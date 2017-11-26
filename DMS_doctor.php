5cce6421f21167dea22530c14af5d62b081fe6af
Tannia Rodriguez <tanniarodriguez@github.com>
2017-11-26 11:08:09 -0600

    Doctor Review dropdown
    Displays the selected value

--- DMS_doctor.php
+++ DMS_doctor.php
@@ -201,22 +201,69 @@ if (!$query) {
 		{
 				$id = $row['user_id'];
 				$accepted_by_DMS = $row['accepted_by_DMS'];
-								
+				$review_array = array('1', '0');
+				
+							
 				echo "<td> <a href='DMS_ViewApp.php?id= $id '>" .$row['user_id'] . "</a> </td>";
 	
 				echo '
 						<td>'.$row['first_name'].'</td>
-						<td>'.$row['last_name'].'</td>
-
-				<td><select name="application_review_list[]">
-					<option value="0">Noncompetitive</option>
-					<option value="1">Competitive</option>
-				</select></td>
+						<td>'.$row['last_name'].'</td>';
+/*
+				echo '<td><select name = "review">';
+						$sql = "SELECT review FROM student_info WHERE user_id=$id;";
+						        $query = mysql_query($sql, $dbc) or die (mysql_error($dbc));
+								if($row = mysql_fetch_assoc($query)) {
+								          $review = $row['review']; 
+								           foreach($review_array as $value) {
+								         if ($review == $value) { //if the province==the user's setting, make it default 
+								           echo '<option value="'.$value.'" selected="selected">'.$value.'</option>';
+								         //} else { //else, echo it as regular
+								           echo '<option value="'.$value.'">'.$value.'</option>';
+								         }
+								       }
+								    }
+				echo '<select></td>';
 				
-						<td>'.$row['EID'].'</td>
+				*/
+				
+				
+						//check the value of the review field in table 'student_info'
+				$stmt = $dbc->query("SELECT review FROM student_info WHERE user_id=$id;");
+				        $x = $stmt->fetch();
+				
+						if ($x['review']=="2") //if review = 2 (Competitive) in the db, show the correct selected value
+						{
+							echo '<td><select name="application_review_list[]">
+								<option value="review = 0 WHERE user_id='.$row['user_id'].'">N/A</option>
+								<option value="review = 1 WHERE user_id='.$row['user_id'].'">Noncompetitive</option>
+								<option value="review = 2 WHERE user_id='.$row['user_id'].'" selected="selected">Competitive</option>
+							</select></td>';
+						}
+						elseif ($x['review']=="1") //if review = 1 (Noncompetitive) in the db, show the correct selected value
+						{
+							echo'<td><select name="application_review_list[]">
+								<option value="review = 0 WHERE user_id='.$row['user_id'].'">N/A</option>
+								<option value="review = 1 WHERE user_id='.$row['user_id'].'" selected="selected">Noncompetitive</option>
+								<option value="review = 2 WHERE user_id='.$row['user_id'].'">Competitive</option>
+							</select></td>';
+						}
+						else{ //if review = 0 (N/A) in the db, show the correct selected value
+							echo '<td><select name="application_review_list[]">
+								<option value="review = 0 WHERE user_id='.$row['user_id'].'"selected="selected">N/A</option>
+								<option value="review = 1 WHERE user_id='.$row['user_id'].'">Noncompetitive</option>
+								<option value="review = 2 WHERE user_id='.$row['user_id'].'">Competitive</option>
+							</select></td>';								}
+				
+				
+				
+				
+				echo ' 	<td>'.$row['EID'].'</td>
 						<td>'.$row['email'].'</td>
 						<td>'.$row['classification'].'</td>
 						<td>'.$row['major'].'</td>';
+						
+						
 						
 						if ($row['accepted_by_DMS']==0)
 						{
--- DMS_doctor_review.php
+++ DMS_doctor_review.php
@@ -7,8 +7,8 @@ $review= $_POST['application_review_list'];
 foreach($review as $value)
 {
 	require 'DMS_db.php';
-	//user_id is hardcoded right now, but it needs to get the id for each record
-		$sql="UPDATE student_info SET review = $value WHERE user_id=2";
+
+		$sql="UPDATE student_info SET $value";
 		$stmt=$dbc->prepare($sql);
 		$stmt->execute();
 	
