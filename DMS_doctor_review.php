be980998138da6c1073feacd58df924732bcb1db
Tannia Rodriguez <tanniarodriguez@github.com>
2017-12-01 11:24:38 -0600

    format

--- DMS_doctor_review.php
+++ DMS_doctor_review.php
@@ -3,60 +3,57 @@
 //require file containing db string
 require 'DMS_db.php';
 
-if (isset($_POST['save']))
-{
-    # Save-button was clicked
-	$review= $_POST['application_review_list'];
-	//$id= $_POST['user_id'];
-
-	foreach($review as $value)
+	if (isset($_POST['save']))
 	{
-		require 'DMS_db.php';
+  	  # Save-button was clicked
+		$review= $_POST['application_review_list'];
+		
+		foreach($review as $value)
+		{
+			require 'DMS_db.php';
 
-		$sql="UPDATE student_info SET $value";
-		$stmt=$dbc->prepare($sql);
-		$stmt->execute();
-
-		//echo $id;
+			$sql="UPDATE student_info SET $value";
+			$stmt=$dbc->prepare($sql);
+			$stmt->execute();
+		
+		}
 	}
-}
-elseif (isset($_POST['accept']))
-{
-	# Accept-button was clicked
-	$accept= $_POST['application_accept_list'];
-	//$id= $_POST['user_id'];
+	elseif (isset($_POST['accept']))
+	{
+		# Accept-button was clicked
+		$accept= $_POST['application_accept_list'];
+		$accept_list=implode(',',$accept);
+	
+		//check if student is already accepted in the database
+		$stmt = $dbc->query("SELECT * FROM student_info WHERE accepted_by_dms= '1' AND user_id IN ($accept_list)");
+		$x = $stmt->fetch();
 
-	$accept_list=implode(',',$accept);
-	//check if student is already accepted in the database
-	$stmt = $dbc->query("SELECT * FROM student_info WHERE accepted_by_dms= '1' AND user_id IN ($accept_list)");
-	$x = $stmt->fetch();
+		//if the student is already accepted, redirect back to the DMS_doctor.php page along with an indication that there was an error
+		if (count($x['user_id'])>0)
+		{
+			$select_application_id=$_POST['select_application'];
+			header("Location: DMS_doctor.php?error='1'&select_application=$select_application_id");
+			die();
+		}
+		foreach($accept as $value)
+		{
+			require 'DMS_db.php';
 
-	//if the student is already accepted, redirect back to the DMS_doctor.php page along with an indication that there was an error
-	if (count($x['user_id'])>0)
+			$sql="UPDATE student_info SET accepted_by_dms = 1 WHERE user_id= $value";
+			echo $sql;
+			$stmt=$dbc->prepare($sql);
+			$stmt->execute();
+		}
+	}
+	//error handling
+	if (!$stmt)
+	{
+		die ('SQL Error: ' . mysqli_error($dbc));
+	}
+	else
 	{
 		$select_application_id=$_POST['select_application'];
-		header("Location: DMS_doctor.php?error='1'&select_application=$select_application_id");
+		header("Location: DMS_doctor.php?select_application=$select_application_id");
 		die();
 	}
-	foreach($accept as $value)
-	{
-		require 'DMS_db.php';
-
-		$sql="UPDATE student_info SET accepted_by_dms = 1 WHERE user_id= $value";
-		echo $sql;
-		$stmt=$dbc->prepare($sql);
-		$stmt->execute();
-	}
-}
-//error handling
-if (!$stmt)
-{
-	die ('SQL Error: ' . mysqli_error($dbc));
-}
-else
-{
-	$select_application_id=$_POST['select_application'];
-	header("Location: DMS_doctor.php?select_application=$select_application_id");
-	die();
-}
 ?>
