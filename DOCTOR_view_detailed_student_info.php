b126b0b609948c04e9bd27bfe2411c6abe039f31
JESSICA\Jessica <jessicabonner@utexas.edu>
2017-12-12 12:34:55 -0600

    admin header

--- DOCTOR_view_detailed_student_info.php
+++ DOCTOR_view_detailed_student_info.php
@@ -112,7 +112,17 @@
 										<a href="ADMIN_dashboard.php" onclick="w3_close()" class="nav-link">Home</a>
 									</li>
 									<li class="nav-item" role="menuitem">
-										<a href="ADMIN_create_profile.php" onclick="w3_close()" class="nav-link">New Profile</a>
+										<a href="#" class="nav-link has-child nolink">Manage Profiles</a>
+										<div class="sub-nav-wrapper">
+											<div class="sub-nav-row">
+					                                          <ul class="sub-nav">
+					                                                      <li class="sub-nav-item">
+					                      <a href="ADMIN_manage_roles.php" onclick="w3_close()"  class="sub-nav-link"><span>Manage Roles</span></a>                    </li>
+					                                                                        <li class="sub-nav-item">
+					                      <a href="ADMIN_create_profile.php" onclick="w3_close()" class="sub-nav-link"><span>New Profile</span></a>                    </li>
+					                                                                                                                    </ul>
+					            </div>
+										</div>
 									</li>
 									<li class="nav-item" role="menuitem">
 										<a href="ADMIN_create_program.php" onclick="w3_close()" class="nav-link">New Program</a>
@@ -123,7 +133,7 @@
 											<div class="sub-nav-row">
 					                                          <ul class="sub-nav">
 					                                                      <li class="sub-nav-item">
-					                      <a href="ADMIN_create_application.php" onclick="w3_close()"  class="sub-nav-link"><span>New Applications</span></a>                    </li>
+					                      <a href="ADMIN_create_application.php" onclick="w3_close()"  class="sub-nav-link"><span>New Application</span></a>                    </li>
 					                                                                        <li class="sub-nav-item">
 					                      <a href="ADMIN_view_all_active_applications.php" onclick="w3_close()" class="sub-nav-link"><span>Edit Applications</span></a>                    </li>
 					                                                                        <li class="sub-nav-item">
