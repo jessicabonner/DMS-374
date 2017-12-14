cf6b03f33e07a5fb64f83cb2086adc625adf53ad
JESSICA\Jessica <jessicabonner@utexas.edu>
2017-12-14 13:10:04 -0600

    kjhk

--- DMS_general_functions.php
+++ DMS_general_functions.php
@@ -4,7 +4,8 @@
 	set_error_handler("error_redirect");
 	function error_redirect($errno, $errstr)
 	{
-		Header("Location: DMS_error.php");
+		//Header("Location: DMS_error.php");
+		echo "error";
 		die();
 	}
 	
