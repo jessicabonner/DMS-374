c7a7f97c39ddbd95765ac088b858aefe94153ece
JESSICA\Jessica <jessicabonner@utexas.edu>
2017-12-14 17:56:32 -0600

    m

--- DOCTOR_update_review.php
+++ DOCTOR_update_review.php
@@ -3,6 +3,7 @@ require "DMS_general_functions.php";
 $role_id_array=array("2","4");
 require "DMS_authenticate.php";
 $user_id = $_SESSION['user_id'];
+require 'DMS_db.php';
 
 
 //use PHPMailer\PHPMailer\PHPMailer;
@@ -22,7 +23,7 @@ $user_id = $_SESSION['user_id'];
 	
 	//THIS FOR WILL BE LINKED TO DOCTOR_view_detailed_student_info.php TO Change accepted_by_dms
 
-	require 'DMS_db.php';
+	
 
 	$student_id=$_POST['user_id'];
 	$application_id=$_POST['application_id'];
@@ -129,88 +130,7 @@ if ($_SESSION['role']==2)
 
 	
 	
-	//if user clicked submit button named update to save the review field
-	elseif(isset($_POST['update']))
-		{
-			
-			$new_review = $_POST['new_review'];
-			$student_id=$_POST['user_id'];
-			
-			$hours_working_week=$_POST['hours_working_week'];
-			$hourly_rate =$_POST['hourly_rate'];
-
-			// if $new_review exists, update database to set student's review to the new value
-			if( ($_POST['new_review'])!== null)
-				{
-					$new_review = $_POST['new_review'];
-					$sql = "UPDATE review SET $new_review WHERE user_id='".$student_id."' AND application_id=".$_POST['application_id'];
-					$stmt=$dbc->prepare($sql);
-					$stmt->execute();
-				}
-			if( ($_POST['potential'])!== null)
-			{
-					$new_potential = $_POST['potential'];
-					$sql = "UPDATE review SET potential = '".$new_potential."' WHERE user_id= '".$student_id."' AND application_id='".$_POST['application_id']."'";
-					$stmt=$dbc->prepare($sql);
-					$stmt->execute();
-			}
-			elseif( ($_POST['potential'])== null)
-			{
-					
-					$sql = "UPDATE review SET potential=NULL WHERE user_id= '".$student_id."' AND application_id='".$_POST['application_id']."'";
-					$stmt=$dbc->prepare($sql);
-					$stmt->execute();
-			}
-			if( ($_POST['interview'])!== null)
-			{
-					$new_interview = $_POST['interview'];
-					$sql = "UPDATE review SET interview= '".$new_interview."' WHERE user_id= '".$student_id."' AND application_id='".$_POST['application_id']."'";
-					$stmt=$dbc->prepare($sql);
-					$stmt->execute();
-			}
-			elseif( ($_POST['interview'])== null)
-			{
-					
-					$sql = "UPDATE review SET interview=NULL WHERE user_id= '".$student_id."' AND application_id='".$_POST['application_id']."'";
-					$stmt=$dbc->prepare($sql);
-					$stmt->execute();
-			}
-			
-			
-			//$stmt2 = $dbc-> prepare('UPDATE student_info SET hours_working_week = :hours_working_week, hourly_rate = :hourly_rate
-				//WHERE user_id= :student_id');
 	
-			$stmt2 = $dbc-> prepare('UPDATE student_info SET hours_working_week = :hours_working_week, hourly_rate = :hourly_rate
-			WHERE user_id= :student_id');
-		
-			$stmt2->execute(array('hours_working_week' => $hours_working_week, 'student_id' => $student_id, 'hourly_rate' => $hourly_rate,));
-			
-			}
-}
-
-elseif ($_SESSION['role']==4)
-{
-	if( ($_POST['approved'])!== null)
-			{
-					
-					$sql = "UPDATE review SET approved=1 WHERE user_id= '".$student_id."' AND application_id='".$_POST['application_id']."'";
-					$stmt=$dbc->prepare($sql);
-					$stmt->execute();
-					
-					
-			}
-			elseif( ($_POST['approved'])== null)
-			{
-					
-					$sql = "UPDATE review SET approved=NULL WHERE user_id= '".$student_id."' AND application_id='".$_POST['application_id']."'";
-					$stmt=$dbc->prepare($sql);
-					$stmt->execute();
-					
-			}
-			
-			
-}
-			
 	
 			
 	//if the statement fails, display an error
