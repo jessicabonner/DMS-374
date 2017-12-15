667353f79ea49d5f42f0a7e1c33880c69a43d7c8
JESSICA\Jessica <jessicabonner@utexas.edu>
2017-12-14 19:50:49 -0600

    student functions commented

--- STUDENT_functionality.php
+++ STUDENT_functionality.php
@@ -2,6 +2,7 @@
 
 $user_id=$_SESSION['user_id'];
 
+//this function gets all the applcations that the specified user has applied to
 function get_applications_student_applied_to($user_id)
 {
 	require 'DMS_db.php';
@@ -27,6 +28,7 @@ function get_applications_student_applied_to($user_id)
 }
 
 
+//returns the application submit time for s specific user and application combination
 function get_application_submit_date($user_id, $application_id)
 {
 	require 'DMS_db.php';
@@ -42,6 +44,7 @@ function get_application_submit_date($user_id, $application_id)
 	return date('m-d-Y',strtotime($date));
 }
 
+//returns the date when the specified student was accepted to the specified program-- in format (Y-m-d)
 function get_application_accept_date_not_formatted($user_id, $application_id)
 {
 	require 'DMS_db.php';
@@ -58,6 +61,7 @@ function get_application_accept_date_not_formatted($user_id, $application_id)
 }
 
 
+//returns whether or not a student was accepted to the specified program
 function get_accepted($user_id, $application_id)
 {
 	require 'DMS_db.php';
@@ -77,6 +81,7 @@ function get_accepted($user_id, $application_id)
 	}
 }
 
+//returns whether or not a specified student has responded to their offer for the specified application
 function get_accepted_offer($user_id, $application_id)
 {
 	require 'DMS_db.php';
@@ -97,27 +102,7 @@ function get_accepted_offer($user_id, $application_id)
 	}
 }
 
-
-function get_accepted_declined_offer($user_id, $application_id)
-{
-	
-	require 'DMS_db.php';
-	//select all the applications that are active
-	$sql="SELECT student_accept_offer FROM review WHERE user_id='". $user_id ."' AND application_id=$application_id";
-	$stmt=$dbc->prepare($sql);
-	$stmt->execute();
-	$review= $stmt->fetch();
-	
-	if ($review['student_accept_offer']=="0")
-	{
-		return "0";
-	}
-	elseif ($review['student_accept_offer']=="1")
-	{
-		return "1";
-	}
-}
-
+//returns whether 
 function check_student_accepted_offer()
 
 {
@@ -140,6 +125,29 @@ function check_student_accepted_offer()
 	}
 	
 }
+
+//returns whether or not a specified student has accepted to their offer for the specified application
+function get_accepted_declined_offer($user_id, $application_id)
+{
+	
+	require 'DMS_db.php';
+	//select all the applications that are active
+	$sql="SELECT student_accept_offer FROM review WHERE user_id='". $user_id ."' AND application_id=$application_id";
+	$stmt=$dbc->prepare($sql);
+	$stmt->execute();
+	$review= $stmt->fetch();
+	
+	if ($review['student_accept_offer']=="0")
+	{
+		return "0";
+	}
+	elseif ($review['student_accept_offer']=="1")
+	{
+		return "1";
+	}
+}
+
+
 
 
 
