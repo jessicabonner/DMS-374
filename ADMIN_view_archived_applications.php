1f9d84d7396067f0e1b9ffd5a04d28265b4fb49b
JESSICA\Jessica <jessicabonner@utexas.edu>
2017-12-14 23:03:49 -0600

    l

--- ADMIN_view_archived_applications.php
+++ ADMIN_view_archived_applications.php
@@ -86,18 +86,19 @@ if (!$query) {
 
 
 					</tr>';
+		}
 
-
-		}?>
-		</tbody>
+echo "</tbody>
 
 
 	</table>
 	<tr><td><br></td>
-	<td><input type='submit' value='Delete' style="background-color:#bf5700;color:white;text-shadow: #000 0px 0px 1px;"></td>
+	<td><input type='submit' value='Delete' style='background-color:#bf5700;color:white;text-shadow: #000 0px 0px 1px;'></td>
 	<td><b> **Note that clicking delete will PERMANENTLY delete the selected tables along with all applications </b></td>
 	<tr>
 
-</form>
+</form>";
+		?>
+		
 </body>
 </html>
